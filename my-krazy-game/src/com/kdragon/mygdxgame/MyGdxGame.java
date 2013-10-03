675f1389ac5a33869fa26a87d9ae38e4f6acbd23
Ronaldo Barnes <krazydragon@fullsail.com>
2013-10-03 11:15:52 -0700

    added background image

--- my-krazy-game/src/com/kdragon/mygdxgame/MyGdxGame.java
+++ my-krazy-game/src/com/kdragon/mygdxgame/MyGdxGame.java
@@ -4,19 +4,27 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class MyGdxGame implements ApplicationListener {
     // constant useful for logging
     public static final String LOG = MyGdxGame.class.getSimpleName();
  
     // a libgdx helper class that logs the current FPS each second
+    private SpriteBatch batch;
     private FPSLogger fpsLogger;
+    private Texture texture;
  
     @Override
     public void create()
     {
+    	texture = new Texture(Gdx.files.internal("space.png"));
+    	Texture.setEnforcePotImages(false);
+    	batch = new SpriteBatch();
         Gdx.app.log( MyGdxGame.LOG, "Creating game" );
-        fpsLogger = new FPSLogger();
+        //fpsLogger = new FPSLogger();
     }
  
     @Override
@@ -25,6 +33,7 @@ public class MyGdxGame implements ApplicationListener {
         int height )
     {
         Gdx.app.log( MyGdxGame.LOG, "Resizing game to: " + width + " x " + height );
+        
     }
  
     @Override
@@ -33,9 +42,13 @@ public class MyGdxGame implements ApplicationListener {
         // the following code clears the screen with the given RGB color (green)
         Gdx.gl.glClearColor( 0f, 1f, 0f, 1f );
         Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT );
+        
+        batch.begin();
+        batch.draw(texture, 0, 0, Gdx.graphics.getHeight(),Gdx.graphics.getWidth());
+        batch.end();
  
         // output the current FPS
-        fpsLogger.log();
+        //fpsLogger.log();
     }
  
     @Override
@@ -53,6 +66,7 @@ public class MyGdxGame implements ApplicationListener {
     @Override
     public void dispose()
     {
+    	texture.dispose();
         Gdx.app.log( MyGdxGame.LOG, "Disposing game" );
     }
 }
